3b49088ea7b781eee37fcb2809c884d87b548d63
Mossy23 <jm2mossy@gmail.com>
2026-04-29 17:40:05 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// THEME TOGGLE
+const button = document.getElementById("theme-btn");
+button.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+});
+
+// FAQ ACCORDION
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach(function(q) {
+    q.addEventListener("click", function() {
+        this.parentElement.classList.toggle("active");
+    });
+});
